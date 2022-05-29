adf1dbc15e67a966c85e749123ee0e76a2e6011a
AlexMavrodiev <aleksandyr_mavrodiev@abv.bg>
2022-05-29 23:08:10 +0300

    More tasks to MultidimensionalArrays-Exercise

---
+++ C#-Advanced/MultidimensionalArrays-Exercise/10. RadioactiveMutantVampireBunnies/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Linq;
+
+namespace _10._RadioactiveMutantVampireBunnies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rowCount = input[0];
+            int colCount = input[1];
+            char[,] matrix = new char[rowCount, colCount];
+            int startRow = -1;
+            int startCol = -1;
+            bool isAlive = true;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] line = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = line[col];
+                    if (line[col] == 'P')
+                    {
+                        startRow = row;
+                        startCol = col;
+                    }
+                }
+            }
+            string directions = Console.ReadLine();
+            for (int i = 0; i < directions.Length; i++)
+            {
+                if (directions[i] == 'U')
+                {
+                    if (ValidIndex(startRow - 1, startCol, rowCount, colCount))
+                    {
+                        if (matrix[startRow - 1, startCol] == '.')
+                        {
+                            
+                            startRow -= 1;
+                            PopulateBunnies(matrix, rowCount, colCount);
+                        }
+                        else
+                        {
+                            isAlive = false;
+                            startRow -= 1;
+                            PopulateBunnies(matrix, rowCount, colCount);
+                            break;
+                        }
+                        
+                    }
+                }
+                else if (directions[i] == 'L')
+                {
+                    if (ValidIndex(startRow, startCol - 1, rowCount, colCount))
+                    {
+                        if (matrix[startRow, startCol - 1] == '.')
+                        {
+                            
+                            startCol -= 1;
+                            PopulateBunnies(matrix, rowCount, colCount);
+                        }
+                        else
+                        {
+                            isAlive = false;
+                            startCol -= 1;
+                            PopulateBunnies(matrix, rowCount, colCount);
+                            break;
+                        }
+                        
+                    }
+                }
+                else if (directions[i] == 'R')
+                {
+                    if (ValidIndex(startRow, startCol + 1, rowCount, colCount))
+                    {
+                        if (matrix[startRow, startCol + 1] == '.')
+                        {
+                            
+                            startCol += 1;
+                            PopulateBunnies(matrix, rowCount, colCount);
+                        }
+                        else
+                        {
+                            isAlive = false;
+                            startCol += 1;
+                            PopulateBunnies(matrix, rowCount, colCount);
+                            break;
+                        }
+                        
+                    }
+                }
+                else if (directions[i] == 'D')
+                {
+                    if (ValidIndex(startRow + 1, startCol, rowCount, colCount))
+                    {
+                        if (matrix[startRow + 1, startCol] == '.')
+                        {
+                            
+                            startRow += 1;
+                            PopulateBunnies(matrix, rowCount, colCount);
+                        }
+                        else
+                        {
+                            isAlive = false;
+                            startRow += 1;
+                            PopulateBunnies(matrix, rowCount, colCount);
+                            break;
+                        }
+                        
+                    }
+                }
+            }
+            PrintMatrix(matrix);
+            if (isAlive)
+            {
+                Console.WriteLine($"won: {startRow} {startCol}");
+            }
+            else
+            {
+                Console.WriteLine($"dead: {startRow} {startCol}");
+            }
+        }
+
+        static void PopulateBunnies(char[,] matrix, int rowSize, int colSize)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == 'B')
+                    {
+                        if (ValidIndex(row, col - 1, rowSize, colSize))
+                        {
+                            matrix[row, col - 1] = 'B';
+                        }
+                        if (ValidIndex(row - 1, col, rowSize, colSize))
+                        {
+                            matrix[row - 1, col] = 'B';
+                        }
+                        if (ValidIndex(row, col + 1, rowSize, colSize))
+                        {
+                            matrix[row, col + 1] = 'B';
+                        }
+                        if (ValidIndex(row + 1, col, rowSize, colSize))
+                        {
+                            matrix[row + 1, col] = 'B';
+                        }
+                    }
+                }
+            }
+        }
+
+        static bool ValidIndex(int row, int col, int rowSize, int colSize)
+        {
+            if (row < 0 || row >= rowSize || col < 0 || col >= colSize)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C#-Advanced/MultidimensionalArrays-Exercise/9. Miner/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Linq;
+
+namespace _9._Miner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            string[] commands = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            char[,] matrix = new char[size, size];
+            int coalCount = 0;
+            int startRow = -1;
+            int startCol = -1;
+            for (int row = 0; row < size; row++)
+            {
+                char[] line = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = line[col];
+                    if (line[col] == 'c')
+                    {
+                        coalCount++;
+                    }
+                    if (line[col] == 's')
+                    {
+                        startRow = row;
+                        startCol = col;
+                    }
+                }
+            }
+
+            for (int i = 0; i < commands.Length; i++)
+            {
+                if (commands[i] == "up")
+                {
+                    int newRow = startRow - 1;
+                    int newCol = startCol;
+                    if (ValidateIndex(size, newRow, newCol))
+                    {
+                        startRow -= 1;
+               
+                        if (matrix[startRow, startCol] == 'c')
+                        {
+                            coalCount--;
+                            matrix[startRow, startCol] = '*';
+                        }
+                        if (matrix[startRow, startCol] == 'e')
+                        {
+                            Console.WriteLine($"Game over! ({startRow}, {startCol})");
+                            Environment.Exit(0);
+                        }
+                        if (coalCount <= 0)
+                        {
+                            Console.WriteLine($"You collected all coals! ({startRow}, {startCol})");
+                            Environment.Exit(0);
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (commands[i] == "left")
+                {
+                    int newRow = startRow;
+                    int newCol = startCol - 1;
+                    if (ValidateIndex(size, newRow, newCol))
+                    {
+                        startCol -= 1;
+                        if (matrix[startRow, startCol] == 'c')
+                        {
+                            coalCount--;
+                            matrix[startRow, startCol] = '*';
+                        }
+                        if (matrix[startRow, startCol] == 'e')
+                        {
+                            Console.WriteLine($"Game over! ({startRow}, {startCol})");
+                            Environment.Exit(0);
+                        }
+                        if (coalCount <= 0)
+                        {
+                            Console.WriteLine($"You collected all coals! ({startRow}, {startCol})");
+                            Environment.Exit(0);
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (commands[i] == "right")
+                {
+                    int newRow = startRow;
+                    int newCol = startCol + 1;
+                    if (ValidateIndex(size, newRow, newCol))
+                    {
+                        startCol += 1;
+                        if (matrix[startRow, startCol] == 'c')
+                        {
+                            coalCount--;
+                            matrix[startRow, startCol] = '*';
+                        }
+                        if (matrix[startRow, startCol] == 'e')
+                        {
+                            Console.WriteLine($"Game over! ({startRow}, {startCol})");
+                            Environment.Exit(0);
+                        }
+                        if (coalCount <= 0)
+                        {
+                            Console.WriteLine($"You collected all coals! ({startRow}, {startCol})");
+                            Environment.Exit(0);
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (commands[i] == "down")
+                {
+                    int newRow = startRow + 1;
+                    int newCol = startCol;
+                    if (ValidateIndex(size, newRow, newCol))
+                    {
+                        startRow += 1;
+
+                        if (matrix[startRow, startCol] == 'c')
+                        {
+                            coalCount--;
+                            matrix[startRow, startCol] = '*';
+                        }
+                        if (matrix[startRow, startCol] == 'e')
+                        {
+                            Console.WriteLine($"Game over! ({startRow}, {startCol})");
+                            Environment.Exit(0);
+                        }
+                        if (coalCount <= 0)
+                        {
+                            Console.WriteLine($"You collected all coals! ({startRow}, {startCol})");
+                            Environment.Exit(0);
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+            Console.WriteLine($"{coalCount} coals left. ({startRow}, {startCol})");
+        }
+
+        static bool ValidateIndex(int size, int row, int col)
+        {
+            if (row < 0 || row >= size || col < 0 || col >= size)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
