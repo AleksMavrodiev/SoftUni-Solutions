8be71c39448b47f8384ee734695496561b34d154
AlexMavrodiev <aleksandyr_mavrodiev@abv.bg>
2022-06-08 17:17:14 +0300

    Added tasks to the StreamsFilesExcercise

---
+++ C#-Advanced/StreamsFilesExcercise/1. EvenLines/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+                int counter = -1;
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    
+                    counter++;
+                    if (counter % 2 == 0)
+                    {
+                        line = Replace(line);
+                        line = Reverse(line);
+                        return line;
+                    }
+                }
+                return null;
+            }
+        }
+
+        private static string Reverse(string line)
+        {
+            return String.Join(" ", line.Split().Reverse());
+        }
+
+        private static string Replace(string line)
+        {
+            return line.Replace("-", "@").Replace(",", "@").Replace(".", "@").Replace("!", "@").Replace("?", "@");
+        }
+    }
+}
---
+++ C#-Advanced/StreamsFilesExcercise/2. LineNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] lines = File.ReadAllLines(inputFilePath);
+            StreamWriter writer = new StreamWriter(outputFilePath);
+            using (writer)
+            {
+                int count = 0;
+                foreach (var line in lines)
+                {
+                    count++;
+                    int letterCount = line.Count(char.IsLetter);
+                    int signCount = line.Count(char.IsPunctuation);
+                    string modifiedLine = $"Line {count}: {line} ({letterCount})({signCount})";
+                    writer.WriteLine(modifiedLine);
+                }
+            }
+        }
+    }
+}
