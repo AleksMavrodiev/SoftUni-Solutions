06aa3416fb5b5c61203def6cae9cb31d74e2bb1e
Alexander Mavrodiev <aleksandyr_mavrodiev@abv.bg>
2022-07-01 17:35:53 +0300

    Adding solutions for the tasks in Inheritance Lab @SoftUni

---
+++ C#-OOP/InheritanceLab/4. RandomList/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+            list.Add("Abba");
+            list.Add("Muse");
+            list.Add("Coldplay");
+            list.ForEach(x => Console.WriteLine(x));
+            list.RandomString();
+            list.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
