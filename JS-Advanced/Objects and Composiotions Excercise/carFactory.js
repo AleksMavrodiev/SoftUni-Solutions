1408cb37f985e7db93b52a38931a9a9bb925a57b
Alexander Mavrodiev <aleksandyr_mavrodiev@abv.bg>
2022-09-30 09:58:39 +0300

    Objects and Compositions Excercise Initial commit

---
+++ JS-Advanced/Objects and Composiotions Excercise/carFactory.js
@@ -0,0 +1,46 @@
+function carFactory(carRequirements){
+    let result = {};
+    let smallEngine = {
+        power: 90,
+        volume: 1800
+    };
+
+    let normalEngine = {
+        power: 120,
+        volume: 2400
+    };
+
+    let monsterEngine = {
+        power: 200,
+        volume: 3500
+    }
+
+    result["model"] = carRequirements["model"];
+    if(carRequirements.power <= 90){
+        result.engine = smallEngine;
+    }
+    else if(carRequirements.power <= 120){
+        result.engine = normalEngine;
+    }
+    else if(carRequirements.power <= 200){
+        result.engine = monsterEngine;
+    }
+
+    result.carriage = {
+        type: carRequirements["carriage"],
+        color: carRequirements["color"]
+    }
+
+    let wheelSize = carRequirements.wheelsize % 2 == 0 ? carRequirements.wheelsize - 1 : carRequirements.wheelsize;
+
+    result.wheelsize = new Array(4).fill(wheelSize);
+    
+    console.log(result);
+}
+
+carFactory({ model: 'VW Golf II',
+power: 90,
+color: 'blue',
+carriage: 'hatchback',
+wheelsize: 14 }
+);
