60dce14a6ae8bb77bc1eb3c8b4beb2e81b8d45c1
Alexander Mavrodiev <aleksandyr_mavrodiev@abv.bg>
2022-10-11 11:58:06 +0300

    Dom Excercise Show it Task

--- JS-Advanced/JS-Advanced-DOM-Events-Exercise/03. Locked-Profile/app.js
+++ JS-Advanced/JS-Advanced-DOM-Events-Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,22 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = Array.from(document.querySelectorAll("button"));
+    for(let button of buttons){
+        button.addEventListener('click', showMore)
+    }
+
+    function showMore(event){
+        let radioButtons = Array.from(event.target.parentElement.querySelectorAll("input[type=radio]"));
+        let hiddentContents = event.target.parentElement.querySelectorAll("div")[0];
+        if(!radioButtons[0].checked && event.target.innerText == "Show more"){
+            event.target.textContent = "Hide it";
+            hiddentContents.style.display = 'block';
+        }
+        else if(!radioButtons[0].checked && event.target.innerText == "Hide it"){
+            event.target.textContent = "Show more";
+            hiddentContents.style.display = 'none';
+        }
+        else{
+            return;
+        }
+    }
 }
