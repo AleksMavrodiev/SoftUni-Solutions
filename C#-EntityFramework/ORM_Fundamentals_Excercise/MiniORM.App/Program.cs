c0867ffe850398e4c12597a500ca956631cb7c0c
Alexander Mavrodiev <aleksandyr_mavrodiev@abv.bg>
2023-03-05 11:04:11 +0200

    ORM Framework DbContext Fix

---
+++ C#-EntityFramework/ORM_Fundamentals_Excercise/MiniORM.App/Program.cs
@@ -0,0 +1,18 @@
+﻿using MiniORM.App.Data;
+using MiniORM.App.Data.Entities;
+
+var connectionString = @"Server=DESKTOP-KCHC2S4;Database=MinionsDB;Integrated security=true;TrustServerCertificate=true;";
+var context = new SoftUniDbContextClass(connectionString);
+
+context.Employees.Add(new Employee()
+{
+    FirstName = "Gosho",
+    LastName = "Inserted",
+    DepartmentId = context.Departments.First().Id,
+    IsEmployed = true
+});
+
+var employee =  context.Employees.Last();
+employee.FirstName = "Modified";
+
+context.SaveChanges();
