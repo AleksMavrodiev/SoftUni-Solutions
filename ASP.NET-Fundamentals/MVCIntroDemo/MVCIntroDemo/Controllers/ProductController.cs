1c63db2274ea322b66f990378cee6d90c598230e
Alexander Mavrodiev <aleksandyr_mavrodiev@abv.bg>
2023-06-06 21:17:06 +0300

    ASP.NEt Into Excercise

---
+++ ASP.NET-Fundamentals/MVCIntroDemo/MVCIntroDemo/Controllers/ProductController.cs
@@ -0,0 +1,89 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using MVCIntroDemo.Models.Product;
+using Newtonsoft.Json;
+using System.Text.Json;
+using Microsoft.Net.Http.Headers;
+
+namespace MVCIntroDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IEnumerable<ProductViewModel> products = new List<ProductViewModel>()
+        {
+            new ProductViewModel()
+            {
+                Id = 1,
+                Name = "Cheese",
+                Price = 7.00
+            },
+            new ProductViewModel()
+            {
+                Id = 2,
+                Name = "Ham",
+                Price = 5.50
+            },
+            new ProductViewModel()
+            {
+                Id = 3,
+                Name = "Bread",
+                Price = 1.50
+            }
+        };
+
+        [ActionName("My-products")]
+        public IActionResult All(string keyword)
+        {
+            if (keyword != null)
+            {
+                var foundProducts = products.Where(p => p.Name.ToLower().Contains(keyword.ToLower()));
+                return View(foundProducts);
+            }
+            return View(products);
+        }
+
+        public IActionResult ById(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return BadRequest();
+            }
+
+            return View(product);
+        }
+
+        public IActionResult AllAsJson()
+        {
+            return Json(products, new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            });
+        }
+
+        public IActionResult AllAsText()
+        {
+            var text = string.Empty;
+            foreach (var item in products)
+            {
+                text += $"{item.Id}: {item.Name} - {item.Price} lv.";
+                text += "\r\n";
+            }
+
+            return Content(text);
+        }
+
+        public IActionResult AllAsTextFile()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var product in products)
+            {
+                sb.AppendLine($"{product.Id}: {product.Name} - {product.Price} lv.");
+            }
+
+            Response.Headers.Add(HeaderNames.ContentDisposition, @"attachment;filename=products.txt");
+
+            return File(Encoding.UTF8.GetBytes(sb.ToString().TrimEnd()), "text/plain");
+        }
+    }
+}
---
+++ ASP.NET-Fundamentals/MVCIntroDemo/TextSplitterApp/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TextSplitterApp.Models;
+
+namespace TextSplitterApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public IActionResult Index(TextViewModel textViewModel)
+        {
+            return View(textViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Split(TextViewModel textViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.RedirectToAction("Index", textViewModel);
+            }
+
+            string[] words = textViewModel.SplitText
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            string splitText = String.Join(Environment.NewLine, words);
+            textViewModel.SplitText = splitText;
+
+            return this.RedirectToAction("Index", textViewModel);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
