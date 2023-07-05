8972cd49a6ff455c3d71a3c9fa6d2dfc4475a6be
Alexander Mavrodiev <aleksandyr_mavrodiev@abv.bg>
2023-07-05 20:28:10 +0300

    MergeSortAndGreedyAlgorithmsAdded

---
+++ C#-Algorithms/SearchingSortingandGreedyAlgorithms-Lab/MergeSort/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace MergeSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Console.WriteLine(string.Join(" ", MergeSort(numbers)));
+        }
+
+        public static int[] MergeSort(int[] array)
+        {
+            if (array.Length == 1)
+            {
+                return array;
+            }
+
+            int middleIdx = array.Length / 2;
+            var lefthalf = array.Take(middleIdx).ToArray();
+            var rightHalf = array.Skip(middleIdx).ToArray();
+
+            return MergeArrays(MergeSort(lefthalf), MergeSort(rightHalf));
+        }
+
+        private static int[] MergeArrays(int[] left, int[] right)
+        {
+            var sorted = new int[left.Length + right.Length];
+            var sortedIdx = 0;
+            var leftIdx = 0;
+            var rightIdx = 0;
+            while (leftIdx < left.Length && rightIdx < right.Length)
+            {
+                if (left[leftIdx] < right[rightIdx])
+                {
+                    sorted[sortedIdx++] = left[leftIdx++];
+                }
+                else
+                {
+                    sorted[sortedIdx++] = right[rightIdx++];
+                }
+            }
+
+            while (leftIdx < left.Length)
+            {
+                sorted[sortedIdx++] = left[leftIdx++];
+            }
+
+            while (rightIdx < right.Length)
+            {
+                sorted[sortedIdx++] = right[rightIdx++];
+            }
+
+            return sorted;
+        }
+    }
+}
